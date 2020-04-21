41f67b18b82244ee537c3d42d6f680322ab006dc
Manjunath Koralli <mskoralli@gmail.com>
2020-04-21 15:28:55 +0530

    added navigation rule

--- src/com/shop/product/UserManager.java
+++ src/com/shop/product/UserManager.java
@@ -31,25 +31,40 @@ public class UserManager implements Serializable{
     public String signIn(String username, String password) {
         User user = userService.getUser(username);
         if (user == null || !password.equals(user.getPassword())) {
-            return "sign-in";
+        	//without configuring in faces-config.xml
+            //return "sign-in"; 
+        	
+        	//if configured in faces-config.xml
+        	return "failure";
         }
 
         currentUser = user;
-        return "index";
+        //without configuring in faces-config.xml
+        //return "index";
+        
+        //if configured in faces-config.xml
+        return "success";
     }
 
     public String signOut() {
         // End the session, removing any session state, including the current user and content of the shopping cart
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 
-        // Redirect is necessary to let the browser make a new GET request
+        /* Redirect is necessary to let the browser make a new GET request */
         return "index?faces-redirect=true";
+        //without configuring in faces-config.xml return type of method will be STRING
+        
+        //if configured in faces-config.xml return type will be void
     }
 
     public String save(User user) {
         userService.saveUser(user);
         currentUser = user;
-        return "index";
+        //without configuring in faces-config.xml
+        //return "index";
+        
+        //if configured in faces-config.xml
+        return "home";
     }
 	
 }
